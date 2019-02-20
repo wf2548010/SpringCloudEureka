91436aecb32731d188f39aef77ab123c2edfe3fc
wf2548010 <32317716+wf2548010@users.noreply.github.com>
2019-02-20 11:15:33 +0800

    服务消费、负载均衡、断路器。

---
+++ service-ribbon/src/main/java/com/winning/service/ServiceRibbonApplication.java
@@ -0,0 +1,30 @@
+package com.winning.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+//加@EnableHystrix开启熔断
+@EnableHystrix
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+
+	@Bean
+	//表明这个restTemplate开启负载均衡的功能
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
+
